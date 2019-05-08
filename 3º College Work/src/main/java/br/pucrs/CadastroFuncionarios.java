e7a7dbb16d75a57840a27c0daae01a54f5ed49a7
18200285 <mhrymalaksouza@gmail.com>
2019-05-08 18:56:42 -0300

    em produção

---
+++ 3º College Work/src/main/java/br/pucrs/CadastroFuncionarios.java
@@ -0,0 +1,102 @@
+package br.pucrs;
+import java.io.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+
+import java.nio.charset.Charset;
+
+import java.util.ArrayList;
+
+public class CadastroFuncionarios {
+
+    private ArrayList<Funcionario> lista;
+
+    private static CadastroFuncionarios cad = null;
+
+    private CadastroFuncionarios() {
+        lista = new ArrayList<>();
+    }
+
+    public static CadastroFuncionarios getInstance() {
+        if (cad == null) {
+            cad = new CadastroFuncionarios();
+        }
+        return cad;
+    }
+
+    public void cleanAll() { lista = new ArrayList<>(); }
+
+    public boolean add(Funcionario f) {
+        return lista.add(f);
+    }
+
+    public boolean saveFile(String nomeArq) {
+        Path path1 = Paths.get(nomeArq);
+        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path1, Charset.forName("utf8")))) {
+            for(Funcionario f: lista)
+                writer.format("%d;%s;%.1f%n", f.getCodigo(),
+                        f.getNome(), f.getSalarioBruto());
+        }
+        catch (IOException x) {
+            System.err.format("Erro de E/S: %s%n", x);
+        }
+        return true;
+    }
+    public boolean saveBinaryFile(String nomeArq) {
+        Path path1 = Paths.get(nomeArq);
+        try (ObjectOutputStream writer = new ObjectOutputStream(Files.newOutputStream(path1))) {
+            for(Funcionario f: lista)
+                writer.writeObject(f.toString());
+        }
+        catch (IOException x) {
+            System.err.format("Erro de E/S: %s%n", x);
+        }
+        return true;
+    }
+
+    public boolean readFile(String nomeArq) {
+        Path path1 = Paths.get(nomeArq);
+        try (BufferedReader reader = Files.newBufferedReader(path1, Charset.forName("utf8"))) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                String[] dados = line.split(";");
+                int cod = Integer.parseInt(dados[0]);
+                double sal = Double.parseDouble(dados[2].replace(",","."));
+                Funcionario f = new Funcionario(cod,dados[1],sal);
+                cad.add(f);
+            }
+        }
+        catch (IOException x) {
+            System.err.format("Erro de E/S: %s%n", x);
+        }
+        return true;
+    }
+
+    public boolean readBinaryFile(String nomeArq) {
+        Path path1 = Paths.get(nomeArq);
+        Funcionario funcionario = null;
+
+        try (ObjectInputStream reader = new ObjectInputStream(path1) {
+            funcionario = (Funcionario) reader.readObject();
+        }
+        catch (IOException x) {
+            System.err.format("Erro de E/S: %s%n", x);
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CadastroFuncionarios{" + "lista=" + lista + '}';
+    }
+
+    public String relatorio() {
+        StringBuilder rel = new StringBuilder("");
+        for (Funcionario f : lista) {
+            rel.append(f.toString());
+            rel.append("\n");
+        }
+        return rel.toString();
+    }
+}
